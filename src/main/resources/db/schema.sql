305c5232f974221e5a7e95c7ac476948d8ea7d50
haungwu <1065774387@qq.com>
2019-09-23 21:51:28 +0800

    updata file.flt 9.23

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -13,6 +13,18 @@ CREATE TABLE `user` (
   `avatar_url` varchar(200) DEFAULT '' comment '头像',
   PRIMARY KEY (`id`)
 );
+DROP TABLE IF EXISTS `student`;
+CREATE TABLE `student` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) DEFAULT NULL comment '用户名',
+  `password` varchar(255) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `phone` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
 ALTER TABLE `user` ADD UNIQUE (`username`);
 ALTER TABLE `user` ADD UNIQUE (`email`);
 ALTER TABLE `user` ADD UNIQUE (`phone`);
+ALTER TABLE `student` ADD UNIQUE (`username`);
+ALTER TABLE `student` ADD UNIQUE (`email`);
+ALTER TABLE `student` ADD UNIQUE (`phone`);
